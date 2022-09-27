563ab1dfe988c337b36eb35dc615816590304029
Nicolás Piován <nicolaspiovandev@gmail.com>
2022-09-27 20:31:17 -0300

    rutas finalizadas

--- index.js
+++ index.js
@@ -5,75 +5,24 @@ class Contenedor {
         this.file = file;
     }
 
-    async save(product) {
-        let content = await fs.promises.readFile(this.file)
-        let contObj = JSON.parse(content) //transformacion de string a obj
-        let newId;
-        newId = contObj.length > 0 ? contObj.length + 1 : 1; //if else resumido
-        /* if (contObj.length > 0) {
-            newId = contObj.length + 1;
-        }else{
-            newId =1
-        } */
-        product.id = newId;
-        contObj.push(product) //agrego el objeto al array
-        await fs.promises.writeFile(this.file, JSON.stringify(contObj)) //transformacion de obj a string
-
-    }
 
     async getAll() {
         let content = await fs.promises.readFile(this.file)
-        let contObj = JSON.parse(content) //transformacion de string a obj
-        return contObj
+        /* let contObj = JSON.parse(content) */ //transformacion de string a obj
+        return content
     }
-    async getById(id) {
-        let contObj = await this.getAll()
-        let result = contObj.find(obj => obj.id == id)
-        return result
-    }
-    async deleteById(id) {
-        let contObj = await this.getAll()
-        let newObj = contObj.find(obj => obj.id !== id)
-        await fs.promises.writeFile(this.file, JSON.stringify(newObj))
-    }
-    async deleteAll() {
-        await fs.promises.writeFile(this.file, "[]")
-    }
+
 }
 
 let products = new Contenedor('products.txt')
 
-let product1 = {
-    "name": "Vans shoes",
-    "price": 70,
-    "thumbnail": "https://www.digitalsport.com.ar/files/products/60b685306b1db-486140-500x500.jpg"
-}
-
-let product2 = {
-    "name": "Nike shoes",
-    "price": 85,
-    "thumbnail": "https://www.digitalsport.com.ar/files/products/625dfa1392383-547438-500x500.jpg"
-}
-
-let product3 = {
-    "name": "Adidas shoes",
-    "price": 65,
-    "thumbnail": "https://www.digitalsport.com.ar/files/products/612041815a11a-539116-500x500.jpg"
-}
 
 const useContainer = async () => {
-    await products.save(product1)
-    await products.save(product2)
-    await products.save(product3)
+    await products.getAll()
+    return products
 }
 
 
 useContainer()
 
-
-/* products.getById()
-
-products.deleteById()
-
-products.deleteAll() */
 module.exports = Contenedor
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express')
-const app = express()
 const Contenedor = require('./index')
+const app = express()
 
+let contenedor = new Contenedor("products.txt")
 
-app.get('/productos', (req, res) => {
-   let contenedor = new Contenedor()
-   res.send(`Estos son todos los productos: `)
+const randomProduct = async () => {
+   let totalProducts = await contenedor.getAll() //traigo todos los productos 
+   let randomParse = JSON.parse(totalProducts) //los paso de string a objeto
+   let randomP = await randomParse[Math.floor(Math.random() * randomParse.length)]; //funcion para traer obj al azar
+   return randomP // push
+}
+
+app.get('/productos', async (req, res) => {
+   let products = await contenedor.getAll()
+   res.send(`Estos son todos los productos: ${products}`)
 })
 
-app.get('/productoRandom', (req, res) => {
-   res.send(`El producto elegido al azar es:  `)
+app.get('/productoRandom', async (req, res) => {
+   let productRandom = await randomProduct() //traigo el array 
+   let finalProduct = JSON.stringify(productRandom) //lo vuelvo a pasar d obj a string
+   res.send(`El producto elegido al azar es: ${finalProduct} `)
 })
 
 
