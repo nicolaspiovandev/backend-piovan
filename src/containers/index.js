a0ccc5bb9890e4cf3daacaa31cbdee9f6e246cd8
Nicolás Piován <nicolaspiovandev@gmail.com>
2022-10-10 19:55:02 -0300

    new commit

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,21 +1,32 @@
-class Container{
-    constructor(){
+class Container {
+    constructor() {
         this.productos = []
     }
 
-    save(producto){
+    save(producto) {
         let id = this.productos.length + 1;
         producto.id = id;
         this.productos.push(producto)
     }
 
-    getAll(){
+    getAll() {
         return this.productos;
     }
 
-    getById(id){
-        // retorna producto con id 
+    getById(id) {
+        // retorna producto con id
+        let productFound = this.productos.find(obj => obj.id === +id)
+        return productFound
+    }
+
+    deleteById(id) {
+        this.productos= this.productos.filter(obj => obj.id !== +id)
+    }
+
+    updateById(id, newProduct) {
+        this.productos = this.productos.map(obj => obj.id === id ? {...newProduct, id} : obj)
     }
 }
+
 
 module.exports = Container;
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,13 +1,64 @@
 const { Router } = require('express');
+const Container = require('../containers');
 const routerProduct = Router()
 
+const container = new Container()
 
-routerProduct.get('/', (req, res) => {
-    res.json({ msg: 'Get product'})
+routerProduct.get('/', (req, res, next) => {
+    const productos = container.getAll()
+    res.json({ productos })
 })
 
-routerProduct.post('/', (req, res) => {
-    res.json({ msg: 'Post product'})
+routerProduct.get('/:id', (req, res, next) => {
+    const { id } = req.params
+    const product = container.getById(id)
+    if (!product) {
+        return res.json({
+            msg: "El producto no existe",
+            error: true
+        })
+    }
+    res.json({ data: product })
 })
+
+routerProduct.post('/', (req, res, next) => {
+    const { name, price, thumbnail } = req.body
+    if (!name || !price || !thumbnail) {
+        return res.json({
+            msg: "Faltan datos para guardar el producto",
+            error: true
+        })
+    }
+
+    const product = { name, price, thumbnail }
+    container.save(product)
+
+    res.json({ data: product })
+})
+
+routerProduct.put('/:id', (req, res, next) => {
+    const { id } = req.params
+    const updatedProduct = req.body
+    container.updateById(+id, updatedProduct)
+    res.json({ data: updatedProduct })
+
+})
+
+routerProduct.delete('/:id', (req, res, next) => {
+    const { id } = req.params
+    const product = container.getById(id)
+    if (!product) {
+        return res.json({
+            msg: "El producto no existe",
+            error: true
+        })
+    }
+    container.deleteById(id)
+    res.json({
+        msg: "El producto fue borrado",
+    })
+})
+
+
 
 module.exports = routerProduct;
